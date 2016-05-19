e5e9e8b36258921219457dac4af2f4284723e906
Galter <ehgalter@gmail.com>
2016-05-18 22:09:52 -0300

    Improve Algorithms

--- linear_search.pas
+++ linear_search.pas
@@ -10,18 +10,31 @@ var
 
   function my_linear_search(key: integer; var array_list: array of integer): integer;
   var
-    i, ilast: integer;
+    i, ilast, arrayValue: integer;
   begin
     my_linear_search := -1;
+    arrayValue := 0;
 
-    for i := 0 to length(array_list) -1  do
+    {for i := 0 to length(array_list) -1  do
     begin
       if array_list[i] = key then
       begin
         my_linear_search := i;
         break;
       end;
+    end;}
+
+    i := 0;
+    while (key <> array_list[i]) do
+    begin
+      arrayValue := i + 1;
+      inc(i);
+
+      writeln('Indice do Array: ', i ,' | Valor: ', array_list[i]);
     end;
+
+    if arrayValue <> 0 then
+      my_linear_search := arrayValue;
   end;
 
 begin
