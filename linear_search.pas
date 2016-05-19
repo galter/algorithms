22f75b341fd52e813d034b659035e86498aa050f
Galter <ehgalter@gmail.com>
2016-05-18 21:00:42 -0300

    Stop Search on Key

--- linear_search.pas
+++ linear_search.pas
@@ -1,20 +1,26 @@
 program linear_search;
+const
+  _LEN = 20;
+
 var
-  my_array: array [1..20] of integer  = (3, 1, 4, 7, 10,
+  my_array: array [1.._LEN] of integer  = (3, 1, 4, 7, 10,
                                         20, 17, 19, 15, 16,
                                         18, 2, 5, 6, 8,
                                         9, 11, 13, 12, 14);
 
   function my_linear_search(key: integer; var array_list: array of integer): integer;
   var
-    i: integer;
+    i, ilast: integer;
   begin
     my_linear_search := -1;
 
     for i := 0 to length(array_list) -1  do
     begin
       if array_list[i] = key then
+      begin
         my_linear_search := i;
+        break;
+      end;
     end;
   end;
 
@@ -22,4 +28,3 @@ begin
   writeln('O índice do número 14 é: ', my_linear_search(14, my_array));
   readln;
 end.
- 
