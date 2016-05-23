6d3c9467123c71ab710f9631f533fce9d295d785
Galter <ehgalter@gmail.com>
2016-05-23 12:41:36 -0300

    Solution

--- linear_search.pas
+++ linear_search.pas
@@ -8,36 +8,21 @@ var
                                         18, 2, 5, 6, 8,
                                         9, 11, 13, 12, 14);
 
-  function my_linear_search(key: integer; var array_list: array of integer): integer;
+  function my_linear_search(key, n: integer; var array_list: array of integer): integer;
   var
-    i, ilast, arrayValue: integer;
+    i, ilast: integer;
   begin
     my_linear_search := -1;
-    arrayValue := 0;
-
-    {for i := 0 to length(array_list) -1  do
-    begin
-      if array_list[i] = key then
-      begin
-        my_linear_search := i;
-        break;
-      end;
-    end;}
 
     i := 0;
-    while (key <> array_list[i]) do
-    begin
-      arrayValue := i + 1;
+    while (array_list[i] <> key) and (i < n) do
       inc(i);
 
-      writeln('Indice do Array: ', i ,' | Valor: ', array_list[i]);
-    end;
-
-    if arrayValue <> 0 then
-      my_linear_search := arrayValue;
+    if (i < n) then
+      my_linear_search := i;
   end;
 
 begin
-  writeln('O índice do número 14 é: ', my_linear_search(14, my_array));
+  writeln('O índice é: ', my_linear_search(19, 20, my_array));
   readln;
 end.
