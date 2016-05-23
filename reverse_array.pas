c8a1b2b59d3fd7c7b60e6b12d2386b71657a593e
Galter <ehgalter@gmail.com>
2016-05-23 20:47:56 -0300

    Reverse Array Initial Commit

---
+++ reverse_array.pas
@@ -0,0 +1,29 @@
+program reverse_array;
+
+const
+  _LEN = 10;
+
+var
+  i, z, j, tmp: integer;
+  array_list: array[1.._LEN] of integer = (0, 2, 4, 6, 8, 10, 12, 14, 16, 18);
+
+begin
+
+  i:= 0;
+  z:= length(array_list);
+
+  while (i < (length(array_list) Div 2)) do
+  begin
+    tmp := array_list[i];
+    array_list[i] := array_list[z];
+    array_list[z] := tmp;
+
+    Inc(i);
+    dec(z);
+  end;
+
+  for j := 0 to length(array_list) - 1 do
+     writeln(' ', array_list[j], ' ');
+
+  readln;
+end.
